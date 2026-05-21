efe49e8b91a2f5e42816bb8089a6a725a6fd5165
Sandeepgowda15 <sandeepgowda.sg10@gmail.com>
2026-05-21 11:13:15 +0530

    Removed scripts

--- log-cleanup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-find /var/log -type f -name "*.log" -mtime +7 -exec rm -f {} \;
-
-echo "Old logs deleted"
--- logfileerror.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-logfile="app.log"
-
-grep "ERROR" $logfile > errors.txt
-
-echo "Errors saved in errors.txt"
